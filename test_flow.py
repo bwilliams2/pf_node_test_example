2589f20280046e95cc9d3a2107c050011649cca5
Bryce Williams <bwilliams2@gmail.com>
2024-01-19 09:15:39 -0600

    Update to linked node input name

--- test_flow.py
+++ test_flow.py
@@ -4,4 +4,4 @@ from promptflow import PFClient
 pf = PFClient()
 pf.test("flow/flow.dag.yaml", inputs=({"text": "Test text"}))
 pf.test("flow/flow.dag.yaml", inputs={"input1": "Hello"}, node="echo_object")
-pf.test("flow/flow.dag.yaml", inputs={"echo": "Hello"}, node="echo")
+pf.test("flow/flow.dag.yaml", inputs={"echo_object.output.echo": "hello"}, node="echo")
